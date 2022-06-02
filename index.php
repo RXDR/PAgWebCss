7844f3a553975626a5e307e5b113b642d9f44420
RXDR <araujocarpio@gmail.com>
2022-06-02 09:39:59 -0500

    add proyect

---
+++ index.php
@@ -0,0 +1,323 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Diviplata</title>
+ 
+
+
+  <!-- Favicons -->
+ 
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+ 
+</head>
+
+<body>
+
+  <!-- ======= Top Bar ======= -->
+  <div id="topbar" class="d-none d-lg-flex align-items-center fixed-top">
+    <div class="container d-flex">
+      <div class="contact-info mr-auto">
+        <i class="icofont-envelope"></i> <a href="rdarioaraujoramos@gmail.com">evertsacuna@gmail.com</a>
+        <i class="icofont-phone"></i> +57 3206772870
+      </div>
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="icofont-twitter"></i></a>
+        <a href="#" class="facebook"><i class="icofont-facebook"></i></a>
+        <a href="#" class="instagram"><i class="icofont-instagram"></i></a>
+        <a href="#" class="skype"><i class="icofont-skype"></i></a>
+        <a href="#" class="linkedin"><i class="icofont-linkedin"></i></i></a>
+      </div>
+    </div>
+  </div>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo mr-auto" style="text-shadow:0 0 black ;"><a href="index.html">DIVI<span style="color: #efb810">PLATA</span></a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo mr-auto"><img src="assets/img/logo.png" alt=""></a>-->
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#footer">Contact</a></li>
+          <li id="google_translate_element"> </li>
+        </ul>
+      </nav><!-- .nav-menu -->
+      
+    
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero2" class="d-flex align-items-center">
+  
+  </section>
+  <section id="hero" class="d-flex align-items-center">
+  
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= Featured Services Section ======= -->
+    <section id="featured-services" class="featured-services">
+      
+
+      
+    </section><!-- End Featured Services Section -->
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about section-bg">
+      <div class="container" data-aos="fade-up">
+
+        <div class="section-title">
+          <h2>Conocenos</h2>
+          <h3> <span>Quienes Somos</span></h3>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6" data-aos="zoom-out" data-aos-delay="100">
+            <img src="assets/img/hero-bg.png" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-6 pt-4 pt-lg-0 content d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="100">
+            <h3>Diviplata, recarga tu billetera desde cualquier lugar, fácil y sencillo!.</h3>
+            <p class="font-italic">
+              Nos caracterizamos por la comercialización de divisas que se convierten en un saldo útil para diversas plataformas de trading. Nacimos como resultado de la necesidad global de interactuar en los mercados financieros más grandes del mundo.
+            </p>
+            <ul>
+              <li>
+                <i style="    background-image: url(assets/img/Skrill-Logo.png);
+                background-repeat: no-repeat;
+                width: 101px;
+                height: 70px; background-color: #871b65;"></i>
+                <div>
+                 <H5>SKRILL</H5>
+                  <p>Puedes recargar tu saldo con solo un Click  :<a href="https://wa.me/573206772870?text=Hola,%20estoy%20interesado%20en%20comprar%20saldo%20en%20Skrill!"> AQUI!</a></p>
+                </div>
+              </li>
+              <li>
+                <i style="    background-image: url(assets/img/perfect-money.png);
+                background-repeat: no-repeat;
+                width: 101px;
+                height: 70px;background-color: #f01010;"></i>
+                <div>
+                  <h5>PERFECT MONEY</h5>
+                  <p>Puedes recargar tu saldo con solo un Click  :<a href="https://wa.me/573206772870?text=Hola,%20estoy%20interesado%20en%20comprar%20saldo%20en%20perfect%20money!"> AQUI!</a></p>
+                </div>
+              </li>
+              <li>
+                <i style="    background-image: url(assets/img/usdt.png);
+                background-repeat: no-repeat;
+                width: 101px;
+                height: 70px;background-color:#26a17b;"></i>
+                <div>
+                  <h5>USDT</h5>
+                  <p>Puedes recargar tu saldo con solo un Click  :<a href="https://wa.me/573206772870?text=Hola,%20estoy%20interesado%20en%20comprar%20saldo%20en%20USDT!"> AQUI!</a></p>
+                </div>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+
+      </div>
+   <!-- ======= Portfolio Section ======= -->
+   <section id="portfolio" class="portfolio">
+      <iframe src="https://es.widgets.investing.com/live-currency-cross-rates?theme=lightTheme" width="100%" style="height: 300px;"></iframe><div class="poweredBy" style="font-family: Arial, Helvetica, sans-serif;"></div>
+    
+  </section><!-- End Portfolio Section --> 
+  <section id="main">
+ 
+
+  </section>
+</main><!-- End #main -->
+
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-newsletter">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-lg-6">
+            <h4>Nuestro costo por transacción es de 1.2% </h4>
+            <diV class="section-title">
+            <h2>Consulta tu transacción</h2>
+            
+          </diV>
+   
+            <form action="">
+               <?Php
+
+include_once('./dolar/simple.php');
+$url="https://www.larepublica.co/indicadores-economicos";
+$html = file_get_html($url);
+
+	foreach ($html->find('span.priceIndicator') as $e) {
+    $dolar[] = $e;
+    break;
+
+    echo str_replace("$", "", $dolar[0]);
+        
+			}
+    
+      
+?>
+            <div class="form-group">
+                <label for="exampleInputPassword1"><strong>Precio del dolar</strong></label>
+                <input type="text" class="form-control" id="box2" value="<?php echo $dolar[0]->innerText();?>" readonly />
+              
+              </div>
+              
+              <div class="form-group">
+                <label for="exampleInputPassword1"><strong>Valor</strong></label>
+                <input type="number" class="form-control" id="box" onchange="cambio();" placeholder="Peso colombiano" />
+              <a class="btn btn-primary" style="margin:5px"> Consulta </a>
+              </div>
+              <div  class="form-group">
+                <label for="exampleInputPassword1"><strong>Usd</strong> </label>
+                <p type="text" id="resul" style="font-weight: 900;">
+                </p>
+                
+              </div>
+              <div  class="form-group">
+                <label for="exampleInputPassword1"><strong>Costo </strong> </label>
+                <p id="result" style="font-weight: 900;"></p> <label><strong>Total</strong></label><p id="total" style="font-weight: 900;"></p>
+                
+              </div>
+              
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script>
+      
+window.addEventListener("keypress", function(event){
+    if (event.keyCode == 13){
+        event.preventDefault();
+    }
+}, false);
+  function cambio(){
+    var dol = document.getElementById('box2').value; 
+  var repla="$"; 
+var dolr = dol.replace(repla, '');
+var dola = dolr.replace('.', '');
+  var dolar = parseInt(dola);
+    var re ;   
+    var pe =parseInt(document.getElementById('box').value, 10);
+    re = pe / dolar;
+    var valor= new Intl.NumberFormat('de-US', { style: 'currency', currency: 'USD' }).format(re);
+    document.getElementById('resul').innerHTML= valor;
+calculate();
+
+}
+
+function calculate() {
+  var dol = document.getElementById('box2').value; 
+  var repla="$"; 
+var dolr = dol.replace(repla, '');
+var dola = dolr.replace('.', '');
+  var dolar = parseInt(dola);
+    var myBox1 = parseInt(document.getElementById('box').value,10); 
+    var result = document.getElementById('result'); 
+    var myResult =parseInt( myBox1 /dolar);
+    var number =(myResult *1.2)/100;
+    var sig = new Intl.NumberFormat('de-US', { style: 'currency', currency: 'USD' }).format(number);
+    var tota= myResult - number;
+    var total = new Intl.NumberFormat('de-US', { style: 'currency', currency: 'USD' }).format(tota);
+    document.getElementById('result').innerHTML= sig;
+    document.getElementById('total').innerHTML= total;
+    return false;  
+    }
+      
+   
+      </script>
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Mas <span >Información</span></h3>
+            
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Vinculos</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Inicio</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+            
+            </ul>
+          </div>
+
+        
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Visita Nuestras Redes Sociales</h4>
+            <p></p>
+            <div class="social-links mt-3">
+              <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+       
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-4">
+      <div class="copyright" id="footer">
+        &copy; Copyright <strong><span>WebdevelR-A</span></strong>. All Rights Reserved
+      </div>
+      
+    </div>
+  </footer><!-- End Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/google-finance-api@1.0.0/index.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/google-finance-api@1.0.0/index.min.js"></script>
+  <!-- Polyfill Intl.NumberFormat, its dependencies & `en` locale data -->
+<script src="https://polyfill.io/v3/polyfill.min.js?features=Intl.NumberFormat,Intl.NumberFormat.~locale.en,Intl.PluralRules.~locale.en"></script>
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+ 
+</body>
+<script type="text/javascript">
+  
+</script>
+</html>
